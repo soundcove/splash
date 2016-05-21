cb0229a8b630b5d61fabe50641232fa11a874b6f
Jelani Thompson <jelanithompson@hotmail.com>
2016-05-21 16:14:14 -0400

    Cleaned up phrase transition

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,17 +1,18 @@
 (function() {
-  var messages = [
-    'we\'re working on it',
-    'coming soon',
-    'are you ready yet?',
-    'brotally swagical yo',
-    'soundcove is awesome',
-    'are we there yet?',
-    'get ready to rock July 2016'
-  ];
-  var message = messages[Math.floor(Math.random() * messages.length)];
+    var messages = [
+        'we\'re working on it',
+        'coming soon',
+        'are you ready yet?',
+        'brotally swagical yo',
+        'soundcove is awesome',
+        'are we there yet?',
+        'get ready to rock July 2016'
+    ];
 
-  window.onload = function() {
-    var el = document.querySelector('.message');
-    el.innerText = message;
-  }
+    var message = messages[Math.floor(Math.random() * messages.length)];
+
+    document.addEventListener('DOMContentLoaded', function() {
+        var el = document.querySelector('.message');
+        el.innerText = message;
+    });
 })();
